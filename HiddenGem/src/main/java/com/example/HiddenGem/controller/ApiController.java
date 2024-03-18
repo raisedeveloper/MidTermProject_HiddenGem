29de232996a07cf52943df6b29427c7a7d197c40
develop_yunju <ilove4903@naver.com>
2024-03-18 14:59:05 +0900

    midProject
    HiddenGem-맛집웹사이트제작

--- HiddenGem/src/main/java/com/example/HiddenGem/controller/ApiController.java
+++ HiddenGem/src/main/java/com/example/HiddenGem/controller/ApiController.java
@@ -22,36 +22,5 @@ public class ApiController {
 	public String map() {
 		return "boardC/api";
 	}
-	String location = "대전광역시 유성구 궁동";
-	Float[] coords = findGeoPoint(location);
-
-	System.out.println(location + ": " + coords[0] + ", " + coords[1]);
-	public static Float[] findGeoPoint(String location) {
-
-	    if (location == null)
-	      return null;
-
-	    // setAddress : 변환하려는 주소 (경기도 성남시 분당구 등)
-	    // setLanguate : 인코딩 설정
-	    GeocoderRequest geocoderRequest = new GeocoderRequestBuilder().setAddress(location).setLanguage("ko").getGeocoderRequest();
-
-	    try {
-	        Geocoder geocoder = new Geocoder();
-	        GeocodeResponse geocoderResponse = geocoder.geocode(geocoderRequest);
-
-	        if (geocoderResponse.getStatus() == GeocoderStatus.OK & !geocoderResponse.getResults().isEmpty()) {
-	            GeocoderResult geocoderResult=geocoderResponse.getResults().iterator().next();
-	            LatLng latitudeLongitude = geocoderResult.getGeometry().getLocation();
-
-	            Float[] coords = new Float[2];
-	            coords[0] = latitudeLongitude.getLat().floatValue();
-	            coords[1] = latitudeLongitude.getLng().floatValue();
-	            ​return coords;
-	        }
-	    } catch (IOException ex) {
-	        ex.printStackTrace();
-	    }
-	    return null;
-	}
 }
 
